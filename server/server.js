f6d28915a6c8021bd8cf6bc96407cf1b9a42bc9b
nityanandrai <nityanandtop@gmail.com>
2018-06-16 16:18:27 +0530

    get todo

--- server/server.js
+++ server/server.js
@@ -25,6 +25,15 @@ app.post('/todos', (req, res) => {
   });
 });
 
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (err) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(3000, () => {
   console.log('Server up and running at 3000');
 });
