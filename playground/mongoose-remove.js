82b402de07b83de76d26d209c0966836d60d90ae
nityanandrai <nityanandtop@gmail.com>
2018-06-23 11:01:07 +0530

    Delete added

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,5 @@
+  const {ObjectID} = require('mongodb');
+
+  const {mongoose} = require('./../server/db/mongoose.js');
+  const {Todo} = require('./../server/models/todo.js');
+  const {User} = require('./../server/models/user.js');
