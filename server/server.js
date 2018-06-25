77e5c91b3eeb2d1d60396ccef11bc5f69b084f92
nityanandrai <nityanandtop@gmail.com>
2018-06-25 20:43:24 +0530

    Login feature added

--- server/server.js
+++ server/server.js
@@ -53,6 +53,19 @@ app.get('/user/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.post('/user/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+});
+
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
---
+++ server/tests/seed/seed.js
@@ -0,0 +1,55 @@
+const jwt = require('jsonwebtoken');
+
+const {ObjectID} = require('mongodb');
+const {Todo} = require('./../../models/todo.js');
+const {User} = require('./../../models/user.js');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
+const users = [{
+  _id: userOneId,
+  email: 'r.nitya@iitg.ac.in',
+  password: 'userOnePass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+  }]
+}, {
+  _id: userTwoId,
+  email: 'nitya@iitg.ac.in',
+  password: 'userTwoPass',
+}];
+
+const todos  = [{
+  _id: new ObjectID(),
+  text: "First text todo"
+}, {
+  _id: new ObjectID(),
+  text: "Second text todo",
+  complete: true,
+  completed: 3342
+}];
+
+const populateTodos = (done) => {
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+
+  }).then(() => done());
+};
+
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    var userOne = new User(users[0]).save();
+    var userTwo = new User(users[1]).save();
+
+    return Promise.all([userOne, userTwo])
+  }).then(() => done());
+};
+
+module.exports = {
+  todos,
+  users,
+  populateTodos,
+  populateUsers
+};
